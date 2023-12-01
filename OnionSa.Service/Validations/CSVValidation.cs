a947c906c97877f26c5acf0c871edfd55845d2d5
Posccis <lkzq33@gmail.com>
2023-11-30 23:41:16 -0300

    new(main)=criação da classe de validação do serviço de csv

---
+++ OnionSa.Service/Validations/CSVValidation.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnionSa.Service.Validations
+{
+    public class CSVValidation
+    {
+        public void ValidaLinhaDataTable(DataRow linha) 
+        {
+
+        }
+    }
+}
