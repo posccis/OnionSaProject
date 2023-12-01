87b2bdebff1f6a71c072f4241813fe7fad1bda5d
Posccis <lkzq33@gmail.com>
2023-11-30 23:40:22 -0300

    update(main)=conclusão da classe de validação do serviço de cliente

--- OnionSa.Service/Validations/ClienteValidation.cs
+++ OnionSa.Service/Validations/ClienteValidation.cs
@@ -8,13 +8,12 @@ using System.Threading.Tasks;
 
 namespace OnionSa.Service.Validations
 {
-    public class ProdutoValidation
+    public class ClienteValidation
     {
         public void ValidaObjetoCliente(Cliente cliente) 
         {
             if (cliente == null) throw new OnionSaServiceException("O objeto está nulo ou vazio. Revise os dados inseridos ou entre em contato com o suporte da Onion S.A e tente novamente.");
             if (String.IsNullOrEmpty(cliente.RazaoSocial)) throw new OnionSaServiceException("É necessário informar a razão social do cliente. Revise os dados inseridos e tente novamente.");
-            if (cliente.Cep <= 1 || cliente.Cep.ToString().Length < 8 || cliente.Cep.ToString().Length > 8) throw new OnionSaServiceException("É necessário informar um CEP válido. Revise os dados inseridos e tente novamente.");
             if (cliente.CPFCNPJ.ToString().Length < 11 || cliente.CPFCNPJ.ToString().Length < 14 || cliente.CPFCNPJ.ToString().Length > 14) throw new OnionSaServiceException("É necessário informar um CPF ou CNPJ válido. Revise os dados inseridos e tente novamente.");
         }
 
