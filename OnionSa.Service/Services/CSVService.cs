b51e75179b92ba20175713baefcb2d301e3c3344
Posccis <lkzq33@gmail.com>
2023-11-30 23:39:47 -0300

    new(main)=criação da classe de serviço de csv e criação do método de conversão do csv para datatable

---
+++ OnionSa.Service/Services/CSVService.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Http;
+using OnionSa.Service.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnionSa.Service.Services
+{
+    public class CSVService
+    {
+        public async Task<DataTable> TransformaCSVParaDataTable(IFormFile planilha)
+        {
+            DataTable dt = new DataTable();
+
+            try
+            {
+                using (StreamReader stream = new StreamReader(planilha.OpenReadStream()))
+                {
+                    string[] cabecalhos = stream.ReadLine().Split(',');
+                    foreach (string cabecalho in cabecalhos)
+                    {
+                        dt.Columns.Add(cabecalho);
+                    }
+
+                    while (!stream.EndOfStream)
+                    {
+                        string[] linha = stream.ReadLine().Split(',');
+                        DataRow novaLinha = dt.NewRow();
+                        for (int i = 0; i < cabecalhos.Length; i++)
+                        {
+                            novaLinha[i] = linha[i];
+                        }
+                        dt.Rows.Add(novaLinha);
+                    }
+
+                }
+                return dt;
+            }
+            catch (Exception ex)
+            {
+
+                throw new OnionSaServiceException($"Ocorreu um erro ao tentar ler a planilha. Valide os dados inseridos ou entre em contato com o suporte da Onion S.A e tente novamente.\nMais detalhes: {ex.Message}");
+            }
+        }
+    }
+}
