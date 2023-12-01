7e7e8f585fc5c0de3066746c22783d092a4b71c9
Posccis <lkzq33@gmail.com>
2023-11-30 23:33:04 -0300

    new(main)=Criei a classe DadosCep para receber os dados da api

---
+++ OnionSa.Domain/Models/DadosCep.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnionSa.Domain.Models
+{
+    public class DadosCep
+    {
+        public string Cep { get; set; }
+        public string Logradouro { get; set; }
+        public string Complemento { get; set; }
+        public string Bairro { get; set; }
+        public string Localidade { get; set; }
+        public string UF { get; set; }
+        public int IBGE { get; set; }
+        public int GIA { get; set; }
+        public int DDD { get; set; }
+        public int Siafi { get; set; }
+    }
+
+}
