ab6a136fd49270ce2d7c42aa70976dd1eb946e9a
Posccis <lkzq33@gmail.com>
2023-12-02 03:06:21 -0300

    new(main)=Criei a API e desenvolvi o endpoint de inserção da planilha

---
+++ OnionSA.API/Controllers/PedidosController.cs
@@ -0,0 +1,70 @@
+using Microsoft.AspNetCore.Mvc;
+using OnionSa.Domain.Models;
+using OnionSa.Repository.Interfaces;
+using OnionSa.Service.Exceptions;
+using OnionSa.Service.Services;
+using OnionSa.Service.Validations;
+using System.Data;
+
+namespace OnionSA.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PedidosController : ControllerBase
+    {
+        private readonly IPedidoRepository _repo;
+        private readonly PedidoService _service;
+        private readonly ClienteService _clienteService;
+        public PedidosController(IPedidoRepository repo, IClienteRepository clienteRepo)
+        {
+            _repo = repo;
+            _service = new PedidoService(repo);
+            _clienteService = new ClienteService(clienteRepo);
+        }
+
+        [AcceptVerbs("POST"), Route("onionsa/enviarplanilha")]
+        [HttpPost]
+        public async Task<IActionResult> EnviarPlanilha([FromForm]IFormFile planilha)
+        {
+            CSVService csvService = new CSVService();
+            CSVValidation csvValidation = new CSVValidation();
+            PedidoValidation pedidoValidation = new PedidoValidation();
+            ClienteValidation clienteValidation = new ClienteValidation();
+            List<Cliente> clientes = new List<Cliente>();
+            List<Pedido> pedidos = new List<Pedido>();
+            try
+            {
+                DataTable dt = await csvService.TransformaCSVParaDataTable(planilha);
+                foreach(DataRow linha in dt.Rows)
+                {
+                    var novaLinha = csvService.TrataCamposLinha(linha);
+
+                    csvValidation.ValidaLinhaDataTable(novaLinha, dt.Rows.IndexOf(linha));
+
+                    var cliente = (_clienteService.CriaObjetoCliente(novaLinha));
+                    clienteValidation.ValidaObjetoCliente(cliente);
+                    clientes.Add(cliente);
+
+                    var pedido = _service.CriaObjetoPedido(novaLinha);
+                    pedidoValidation.ValidaObjetoPedido(pedido);
+                    pedidos.Add(pedido);
+
+                }
+
+                _clienteService.AdicionaVariosClientes(clientes);
+                _service.AdicionaVariosPedidos(pedidos);
+
+                return Ok();
+                
+            }
+            catch (OnionSaServiceException onionExcp)
+            {
+                return BadRequest(onionExcp);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest($"Ocorreu um erro ao tentar processar a planinha. Revise os dados enviados e tente novamente.\nMais detalhes:{ex.Message}");
+            }
+        }
+    }
+}
