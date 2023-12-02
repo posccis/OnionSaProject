0c92629baa1d4ebae55b5e75305a87ba64971a63
Posccis <lkzq33@gmail.com>
2023-12-01 23:51:39 -0300

    update(main)=Criei a validação da lista

--- OnionSa.Service/Validations/ClienteValidation.cs
+++ OnionSa.Service/Validations/ClienteValidation.cs
@@ -10,6 +10,10 @@ namespace OnionSa.Service.Validations
 {
     public class ClienteValidation
     {
+        public void ValidaListaClientes(List<Cliente> clientes) 
+        {
+            if(clientes.Count == 0) throw new OnionSaServiceException("A lista enviada está vázia. Revise os dados inseridos ou entre em contato com o suporte da Onion S.A e tente novamente.");
+        }
         public void ValidaObjetoCliente(Cliente cliente) 
         {
             if (cliente == null) throw new OnionSaServiceException("O objeto está nulo ou vazio. Revise os dados inseridos ou entre em contato com o suporte da Onion S.A e tente novamente.");
