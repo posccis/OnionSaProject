d626af7efe12fcd7c9fb0a517b7220a147164e35
Posccis <lkzq33@gmail.com>
2023-12-03 15:47:08 -0300

    new(main)= Criação da classe de DTO de Pedidos

---
+++ OnionSa.Domain/Models/PedidoDTO.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnionSa.Domain.Models
+{
+    public class PedidoDTO
+    {
+        public string Documento { get; set; }
+        public string RazaoSocial { get; set; }
+        public string CEP { get; set; }
+        public string Regiao { get; set; }
+        public int Valor  { get; set; }
+        public double ValorFinal { get; set; }
+        public string Produto { get; set; }
+        public int NumeroDoPedido { get; set; }
+        public DateTime Data { get; set; }
+        public DateTime DataEntrega { get; set; }
+    }
+}
