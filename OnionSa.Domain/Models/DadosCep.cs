2eec55ecf3d99beba6be2453352e193bc6e26cc2
Posccis <lkzq33@gmail.com>
2023-12-03 15:46:33 -0300

    update(main)=permitindo que alguns dos dados sejam nulos

--- OnionSa.Domain/Models/DadosCep.cs
+++ OnionSa.Domain/Models/DadosCep.cs
@@ -10,12 +10,12 @@ namespace OnionSa.Domain.Models
     {
         public string Cep { get; set; }
         public string Logradouro { get; set; }
-        public string Complemento { get; set; }
+        public string? Complemento { get; set; } = string.Empty;
         public string Bairro { get; set; }
         public string Localidade { get; set; }
         public string UF { get; set; }
         public int IBGE { get; set; }
-        public int GIA { get; set; }
+        public int? GIA { get; set; } = 0;
         public int DDD { get; set; }
         public int Siafi { get; set; }
     }
